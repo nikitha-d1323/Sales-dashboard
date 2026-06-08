d0acbd5c973ef88b0edd3131123c0a3dbc904956
nikitha-d1323 <1323nikitha@gmail.com>
2026-06-08 12:03:34 +0530

    added first KPI card total sales

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -8,6 +8,11 @@ root.title("Sales Dashboard")
 root.geometry("900x600")
 root.configure(bg="white")
 
+# --- Data ---
+months = ["Jan","Feb","Mar","Apr","May","Jun",
+          "Jul","Aug","Sep","Oct","Nov","Dec"]
+sales  = [150,180,210,170,230,260,245,290,310,280,320,350]
+
 # --- Header Frame ---
 header_frame = tk.Frame(root, bg="navy", pady=10)
 header_frame.pack(fill="x")
@@ -17,6 +22,26 @@ tk.Label(header_frame,
          text="SALES DASHBOARD",
          font=("Arial", 20, "bold"),
          bg="navy",
+         fg="white").pack()
+
+# --- KPI Frame ---
+kpi_frame = tk.Frame(root, bg="white", pady=10)
+kpi_frame.pack(fill="x", padx=20)
+
+# --- Card 1 : Total Sales ---
+card1 = tk.Frame(kpi_frame, bg="blue", padx=15, pady=10)
+card1.pack(side="left", expand=True, fill="both", padx=10)
+
+tk.Label(card1,
+         text="Total Sales",
+         font=("Arial", 10, "bold"),
+         bg="blue",
+         fg="white").pack()
+
+tk.Label(card1,
+         text=str(sum(sales)) + " units",
+         font=("Arial", 14, "bold"),
+         bg="blue",
          fg="white").pack()
 
 root.mainloop()
