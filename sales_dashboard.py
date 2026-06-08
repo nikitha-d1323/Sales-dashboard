2c8a37d4805e390b0d21c9b46dcaef0e3fb84d7e
nikitha-d1323 <1323nikitha@gmail.com>
2026-06-08 11:09:08 +0530

    added title label

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -8,4 +8,12 @@ root.title("Sales Dashboard")
 root.geometry("900x600")
 root.configure(bg="white")
 
+# --- Title Label ---
+tk.Label(root,
+         text="SALES DASHBOARD",
+         font=("Arial", 20, "bold"),
+         bg="navy",
+         fg="white",
+         pady=10).pack(fill="x")
+
 root.mainloop()
