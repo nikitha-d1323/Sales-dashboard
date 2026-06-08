ffa0504b82895c67707c01164679a958e20b27cf
nikitha-d1323 <1323nikitha@gmail.com>
2026-06-08 11:08:24 +0530

    added background color

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -1,9 +1,11 @@
 # Sales Dashboard Project
 # Built with Python and Tkinter
+
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Sales Dashboard")
 root.geometry("900x600")
+root.configure(bg="white")
 
 root.mainloop()
